6ed1bcfa4b71bef357582414c51e20031fa1d65c
Kalis <charlesranque49@gmail.com>
2026-07-10 20:41:11 +0800

    Add files via upload

---
+++ GodImages.js
@@ -0,0 +1,9 @@
+const GOD_IMAGES = {
+  // Heroes
+  'Tuwaang': 'images/gods/tuwaanggod.jpg',
+  'Lam-Ang': 'images/gods/lamanggod.jpg',
+  
+  // Monsters
+  'Bakunawa': 'images/gods/bakunawagod.jpg',
+  'Engkanto': 'images/gods/engkantogod.jpg'
+};
